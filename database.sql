a89c688a4dce53b076e78f8874ccff7e641f55a9
Carine Casagrande <55366645+carinecasagrande@users.noreply.github.com>
2022-08-15 14:29:09 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,8 +4,7 @@ CREATE TABLE `user` (
     `name` varchar(100) COLLATE utf8_bin NOT NULL,
     `password` varchar(226) COLLATE utf8_bin NOT NULL,
     `profile_image` blob DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `email_UNIQUE` (`email`)
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `pets` (
